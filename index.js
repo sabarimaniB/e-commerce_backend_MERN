a4e37e0c9facc0b78db2e0c7f000ea68ed902abe
sabarimaniB <sabarimani.b2022eee@sece.ac.in>
2024-08-12 09:48:41 +0530

    feat:reference cors

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors"); 
 const userRoutes = require("./routes/UserRoute");
 const products = require("./routes/productRoutes");
 const cart = require("./routes/cartRoutes");
 const order = require("./routes/orderRoutes");
+
 const app = express();
 app.use(express.json());
-app.use(cors());
-
+app.use(cors()); // Use cors middleware
 
 mongoose.connect("mongodb://localhost:27017/e-commerce")
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
 
-app.use("/products",products);
+app.use("/products", products);
 app.use("/user", userRoutes);
-app.use("/cart",cart);
-app.use("/order",order);
+app.use("/cart", cart);
+app.use("/order", order);
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
